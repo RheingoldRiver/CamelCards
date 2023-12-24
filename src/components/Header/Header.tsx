d7e34ca24ee767b0c747ae7792a2d11941ede8f8
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-12-24 07:18:57 -0600

    add aoc link on score

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,11 @@ function Header() {
     <div className="pl-2 pt-2">
       <h1 className={clsx("text-[30px]")}>Welcome to Camel Cards!</h1>
       Click & Drag your hands to reorder bids. You can generate new hands if you give up on this round. Try to optimize
-      your score!
+      your{" "}
+      <a className="text-blue-400 underline" target="_blank" href="https://adventofcode.com/2023/day/7">
+        score
+      </a>
+      !
     </div>
   );
 }
